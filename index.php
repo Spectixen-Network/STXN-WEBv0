67713b843dbb9ab9fe0d2ddf2ab462ee796fbd4b
AppleJamp <enzoscottt3@gmail.com>
2022-04-07 22:46:57 +0200

    start

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>STX-1</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Test</h1>
+</body>
+
+</html>
