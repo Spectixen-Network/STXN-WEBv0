b0657326492ccbd5d5b5a17f5350f87f5c614413
AppleJamp <enzoscottt3@gmail.com>
2022-05-07 00:58:02 +0200

    New design
    Nastínění jak by mohl vypadat frontend.

--- funkce.php
+++ funkce.php
@@ -30,7 +30,7 @@ function db_connection()
 function email_exist($email)
 {
     $connection = db_connection();
-    $dotaz = "SELECT email FROM login WHERE email='" . $email . "'";
+    $dotaz = "SELECT ? FROM ? WHERE email='" . $email . "'";
     $result = mysqli_query($connection, $dotaz);
     if (mysqli_num_rows($result) > 0)
     {
@@ -111,7 +111,7 @@ function active_bar()
     }
     return $act;
 }
-function navbar()
+function navbar_()
 {
     $act = active_bar();
     echo '
@@ -177,6 +177,38 @@ function navbar()
             </div>
         </nav>
         ';
+}
+function navbar()
+{
+    echo '
+        <nav class="sticky-top">
+            <div class="container-fluid nav-bar-content ">
+                <div class="navbar">
+                    <a href="#" class="navbar-brand">
+                        <img src="images/SpectixenNetwork_logo_bez_pozadi_400x400.png" alt="" class="logo">
+                        <span class="navbar-text">Spectixen Network</span>
+                    </a>
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Test 1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Test 2</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Test 3</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Test 4</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Test 5</a>
+                        // </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    ';
 }
 
 function navbar_logedin()
@@ -325,8 +357,10 @@ function vypis_html_start($title, $css_file_pathname_1, $css_file_pathname_2 = "
 
         <head>
             <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="shortcut icon" href="images/SpectixenNetwork_logo_bez_pozadi_400x400.png" type="image/x-icon">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
             <link rel="stylesheet" href="' . $css_file_pathname_1 . '.css">';
     if ($css_file_pathname_2 != "")
     {
@@ -337,9 +371,10 @@ function vypis_html_start($title, $css_file_pathname_1, $css_file_pathname_2 = "
         echo '<link rel="stylesheet" href="' . $css_file_pathname_3 . '.css">';
     }
 
-    echo '  <link rel="icon" href="images/SpectixenNetwork_logo_bez_pozadi.png">
+    echo '
             <title>Spectixen Network | ' . $title . '</title>
         </head>
+        
         <body>
     ';
 }
