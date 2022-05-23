97ed90913494a80c1789f7c62bbb94beb0b8cccc
AppleJamp <enzoscottt3@gmail.com>
2022-05-23 17:06:21 +0200

    Update stx_1.sql

--- else/stx_1.sql
+++ else/stx_1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Ned 08. kvě 2022, 22:13
--- Verze serveru: 10.4.22-MariaDB
--- Verze PHP: 8.1.2
+-- Vytvořeno: Pon 23. kvě 2022, 14:53
+-- Verze serveru: 10.4.24-MariaDB
+-- Verze PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,18 @@ SET time_zone = "+00:00";
 --
 -- Databáze: `stx_1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `banneduser`
+--
+
+CREATE TABLE `banneduser` (
+  `uid` int(10) UNSIGNED NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -43,11 +55,19 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`uid`, `username`, `password`, `email`, `image_path`, `admin`) VALUES
 (1, 'AppleJamp', 'f933ad3d093dc000bcf947b91d8e3c2b00088166820a531386069c39c504e4a7', 'd.pivka.st@spseiostrava.cz', 'images/avatar_red_zoomed.png', 1),
 (2, 'Yura', '79b98a6be9e8e9b7492a8560b66dfe87ee8f162289d9c6bdb2cd4ec057e93e90', 'crazybrumikcz@gmail.com', 'images/SpectixenNetwork_logo.png', 1),
-(3, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@test.test', 'images/noavatar.png', 0);
+(3, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@test.test', 'images/noavatar.png', 0),
+(4, 'test1', '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', 'test1@test1.test1', 'images/noavatar.png', 0),
+(5, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '', 'images/noavatar.png', 1);
 
 --
 -- Indexy pro exportované tabulky
 --
+
+--
+-- Indexy pro tabulku `banneduser`
+--
+ALTER TABLE `banneduser`
+  ADD KEY `uid` (`uid`);
 
 --
 -- Indexy pro tabulku `user`
@@ -63,7 +83,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pro tabulku `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `banneduser`
+--
+ALTER TABLE `banneduser`
+  ADD CONSTRAINT `banneduser_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
