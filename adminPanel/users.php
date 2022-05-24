1de89a7ef403e3edc2211d4310168131ed3d8ba0
AppleJamp <enzoscottt3@gmail.com>
2022-05-24 11:24:35 +0200

    adminPanel:  user list, bere z DB seznam všech userů. Možnost zabanovat, smazat a kouknout na profil, všechny tyto možnosti jsou WIP.  banned user list, teprve začátek, bude třeba dodělat.

--- adminPanel/users.php
+++ adminPanel/users.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include '../funkce.php';
+
 include  'adminPanelFunctions.php';
 
 html_start("Admin", "../css/style");
@@ -15,17 +16,37 @@ echo
             <div class="row">';
 adminSidebar();
 ?>
+
 <div class="col-10">
+    <!-- UserList start -->
     <div class="row">
-        <div class="col-6" style="background-color: red;">
-            <span>
-                <p>AppleJamp</p>
+        <div class="col-4 ms-1 userLists" style="background-color: rgba(83, 83, 83, 0.2); backdrop-filter: blur(10px); height: 75vh; overflow-y: auto">
+            <span class="d-flex justify-content-center mt-1" style="background-color: rgba(83, 83, 83, 0.2); backdrop-filter: blur(10px);">
+                <h2>All Users</h2>
             </span>
-            <span>
-                <p>Yura</p>
-            </span>
+
+            <?php listAllUsers(); ?>
+
         </div>
-        <div class="col-6" style="background-color: blue;">
+        <!-- userList end -->
+
+        <!-- bannedUsersList start -->
+        <div class="col-4 ms-1" style="background-color: rgba(83, 83, 83, 0.2); backdrop-filter: blur(10px); height: 30vh; overflow-y: auto">
+            <div class="column">
+                <div>
+                    <span class="d-flex justify-content-center mt-1" style="background-color: rgba(83, 83, 83, 0.2); backdrop-filter: blur(10px);">
+                        <h2>Banned Users</h2>
+                    </span>
+
+                </div>
+                <div>
+                    test2
+                </div>
+            </div>
+        </div>
+        <!-- bannedUsersList end -->
+
+        <div class="col-4">
 
         </div>
     </div>
@@ -35,3 +56,52 @@ echo '      </div>
         </div>';
 
 html_end();
+
+
+//User list start
+function userListsListUser($userId, $userUsername)
+{
+    echo
+    '
+            <!-- oneUser start -->
+            <div class="mt-1 userList" style="">
+                <div class="row" style="width: 100%; margin: 0;">
+                    <span class="col-8">
+                        <p class="m-0 p-1 col">[ '.$userId.' ] '.$userUsername.'</p>
+                    </span>
+                    <span class="col-4 d-flex align-content-center flex-wrap">
+                        <div class="row">
+                            <span class="col-4">
+                                <a href="#"><i class="bi bi-search"></i></a>
+                            </span>
+                            <span class="col-4">
+                                <a href="/handlers/banUser.php?uid='.$userId.'"><i class="bi bi-hammer col"></i></a>
+                            </span>
+                            <span class="col-4">
+                                <a href="/handlers/deleteUser.php?uid='.$userId.'"><i class="bi bi-trash"></i></a>
+                            </span>
+                        </div>
+                    </span>
+                </div>
+            </div>
+            <!-- oneUser end -->
+    ';
+}
+function listAllUsers()
+{
+    $con = db_connection();
+
+    $query = "SELECT uid, username FROM user;";
+    $result = mysqli_query($con, $query);
+    for($i = 0; $i < mysqli_num_rows($result); $i++)
+    {
+        $row = mysqli_fetch_assoc($result);
+        userListsListUser($row["uid"], $row["username"]);
+    }
+}
+//User list end
+
+function bannedListListUser()
+{
+
+}
