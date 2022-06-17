88186ee7edc9108bf0abfdc3a1131ebaefbee5e8
AppleJamp <enzoscottt3@gmail.com>
2022-06-17 23:49:25 +0200

    Calnedar
    Funkční možnost zobrazení měsíce a týdne, avšak.. Pořád je potřeba datum zadat ručně do kódu.

--- calendar.php
+++ calendar.php
@@ -19,6 +19,9 @@ $daysOfWeekCZ =
         "So",
         "Ne"
     ];
+
+$currentMonth = date("n");
+$currentYear = date("Y");
 ?>
 
 <div class="container-fluid">
@@ -48,7 +51,29 @@ $daysOfWeekCZ =
             </div>
             <div class="col-10 calendar-body">
                 <?php
-                Month(2, 2022);
+                if (isset($_GET["show"]))
+                {
+                    if (strtolower(test_input($_GET["show"])) == "month")
+                    {
+                        Month($currentMonth, $currentYear);
+                    }
+                    elseif (strtolower(test_input($_GET["show"])) == "week")
+                    {
+                        Week(31, 12, 2021);
+                    }
+                    elseif (strtolower(test_input($_GET["show"])) == "day")
+                    {
+                        echo "WIP";
+                    }
+                    else
+                    {
+                        Month($currentMonth, $currentYear);
+                    }
+                }
+                else
+                {
+                    Month($currentMonth, $currentYear);
+                }
                 ?>
             </div>
         </div>
@@ -59,10 +84,12 @@ $daysOfWeekCZ =
 footer();
 html_end();
 
-function oneDay($day, $month, $year)
+function oneDay($day, $month, $year, $week = false)
 {
     $redirect = "window.open('calendarEvent.php?day=$day&month=$month&year=$year', '_blank')";
     $strToDate = strtotime("$day.$month.$year");
+    $date = date("d.m.Y", $strToDate);
+    $currentDate = date("d.m.Y");
     $daysOfWeekCZ =
         [
             1 => "Po",
@@ -77,65 +104,88 @@ function oneDay($day, $month, $year)
 
     echo
     '
-        <div class="col calendar-day" onclick="' . $redirect . '">
-            <div class="d-flex flex-column">
-                <div class="p-2 row calendar-day-header">
-                    <p class="col">' . $day . '.' . $month . '.</p>
-                    <p class="col"></p>
-                    <p class="col">' . $daysOfWeekCZ[date("N", $strToDate)] . '</p>
-                </div>
-                <div class="p-2 calendar-day-body">
-                    <p style="text-align: center; margin: 0;">
-                        test<br>test
-                    </p>
-                </div>
-            </div>
-        </div>
-    ';
-}
-function oneDayNotIncluded($day, $month, $year)
-{
-    $redirect = "window.open('calendarEvent.php?day=$day&month=$month&year=$year', '_blank')";
-    $strToDate = strtotime("$day.$month.$year");
-    $daysOfWeekCZ =
-        [
-            1 => "Po",
-            "Út",
-            "St",
-            "Čt",
-            "Pá",
-            "So",
-            "Ne"
-        ];
-
-
-    echo
-    '
-        <div class="col calendar-day" onclick="' . $redirect . '">
-            <div class="d-flex flex-column">
-                <div class="p-2 row calendar-day-header" style="background-color: #c300ffa0;">
-                    <p class="col">' . $day . '.' . $month . '.</p>
-                    <p class="col"></p>
-                    <p class="col">' . $daysOfWeekCZ[date("N", $strToDate)] . '</p>
-                </div>
-                <div class="p-2 calendar-day-body">
-                    <p style="text-align: center; margin: 0;">
-                        test<br>test
-                    </p>
-                </div>
-            </div>
-        </div>
-    ';
-}
-function oneWeek($numberOfFirstDay, $month, $year)
-{
-
-    echo '<div class="row">';
-    for ($i = $numberOfFirstDay; $i < $numberOfFirstDay + 7; $i++)
+        <div class="col calendar-day ';
+    if ($currentDate == $date)
     {
-        oneDay($i, $month, $year);
+        echo "current-day";
     }
-    echo '</div>';
+    echo '" ';
+    if ($week)
+    {
+        echo 'style="height: 70vh;"';
+    }
+    echo 'onclick="' . $redirect . '">
+            <div class="d-flex flex-column">
+                <div class="p-2 row calendar-day-header ';
+    if ($currentDate == $date)
+    {
+        echo "current-day";
+    }
+    echo '">
+                    <p class="col">' . $day . '.' . $month . '.</p>
+                    <p class="col"></p>
+                    <p class="col">' . $daysOfWeekCZ[date("N", $strToDate)] . '</p>
+                </div>
+                <div class="p-2 calendar-day-body">
+                    <p style="text-align: center; margin: 0;">
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+    ';
+}
+function oneDayNotIncluded($day, $month, $year, $week = false)
+{
+    $redirect = "window.open('calendarEvent.php?day=$day&month=$month&year=$year', '_blank')";
+    $strToDate = strtotime("$day.$month.$year");
+    $date = date("d.m.Y", $strToDate);
+    $currentDate = date("d.m.Y");
+    $daysOfWeekCZ =
+        [
+            1 => "Po",
+            "Út",
+            "St",
+            "Čt",
+            "Pá",
+            "So",
+            "Ne"
+        ];
+
+
+    echo
+    '
+        <div class="col calendar-day ';
+    if ($currentDate == $date)
+    {
+        echo "current-day";
+    }
+    echo '" ';
+    if ($week)
+    {
+        echo 'style="height: 70vh;"';
+    }
+    echo ' onclick="' . $redirect .
+        '">
+            <div class="d-flex flex-column">
+                <div class="p-2 row calendar-day-header ';
+    if ($currentDate == $date)
+    {
+        echo "current-day";
+    }
+    echo '" style="background-color: #c300ffa0;">
+                    <p class="col">' . $day . '.' . $month . '.</p>
+                    <p class="col"></p>
+                    <p class="col">' . $daysOfWeekCZ[date("N", $strToDate)] . '</p>
+                </div>
+                <div class="p-2 calendar-day-body">
+                    <p style="text-align: center; margin: 0;">
+                        test<br>test
+                    </p>
+                </div>
+            </div>
+        </div>
+    ';
 }
 function Month($monthNumber, $yearNumber)
 {
@@ -143,9 +193,9 @@ function Month($monthNumber, $yearNumber)
     $numOfDaysInMonth = cal_days_in_month(CAL_GREGORIAN, $monthNumber, $yearNumber);
     $lastDayDate = strtotime("$numOfDaysInMonth.$monthNumber.$yearNumber");
     $firstDayOrderNumber = date("N", $firstDayDate);
-    $lastDayOrferNumber = date("N", $lastDayDate);
+    $lastDayOrderNumber = date("N", $lastDayDate);
     $daysBefore = $firstDayOrderNumber - 1;
-    $daysAfter = 7 - $lastDayOrferNumber;
+    $daysAfter = 7 - $lastDayOrderNumber;
     if ($daysBefore != 0)
     {
         if ($monthNumber == 1)
@@ -216,4 +266,88 @@ function Month($monthNumber, $yearNumber)
         }
     }
     // ------ End of printing ------
+}
+function Week($dayInWeek, $monthNumber, $yearNumber)
+{
+    $dayDate = strtotime("$dayInWeek.$monthNumber.$yearNumber");
+    $numOfDaysInMonth = cal_days_in_month(CAL_GREGORIAN, $monthNumber, $yearNumber);
+    if ($monthNumber == 1)
+    {
+        $numOfDaysInMonthBefore = cal_days_in_month(CAL_GREGORIAN, 12, $yearNumber - 1);
+    }
+    else
+    {
+        $numOfDaysInMonthBefore = cal_days_in_month(CAL_GREGORIAN, $monthNumber - 1, $yearNumber);
+    }
+    $dayOrderNumber = date("N", $dayDate);
+    $daysBefore = $dayOrderNumber - 1;
+    $daysAfter = 7 - $dayOrderNumber;
+
+
+    echo '<div class="row" style="height: 70vh">';
+    // ------ Days Before ------
+    for ($b = 0; $b < $daysBefore; $b++)
+    {
+        if ($monthNumber == 1)
+        {
+            if (($dayInWeek - ($daysBefore - $b)) <= 0)
+            {
+                oneDay($numOfDaysInMonthBefore + $dayInWeek - ($daysBefore - $b), 12, $yearNumber - 1, true);
+            }
+            else
+            {
+                oneDay($dayInWeek - ($daysBefore - $b), $monthNumber, $yearNumber, true);
+            }
+        }
+        else
+        {
+            if (($dayInWeek - ($daysBefore - $b)) <= 0)
+            {
+                oneDay($numOfDaysInMonthBefore + $dayInWeek - ($daysBefore - $b), $monthNumber - 1, $yearNumber, true);
+            }
+            else
+            {
+                oneDay($dayInWeek - ($daysBefore - $b), $monthNumber, $yearNumber, true);
+            }
+        }
+    }
+    // ------ End Days Before ------
+    oneDay($dayInWeek, $monthNumber, $yearNumber, true);
+    // ------ Days After ------
+    for ($a = 1; $a <= $daysAfter; $a++)
+    {
+        if ($monthNumber == 12)
+            if (($dayInWeek + $a) > $numOfDaysInMonth)
+            {
+                oneDay($dayInWeek + $a - $numOfDaysInMonth, 1, $yearNumber + 1, true);
+            }
+            else
+            {
+                oneDay($dayInWeek + $a, $monthNumber, $yearNumber, true);
+            }
+        else
+        {
+            if (($dayInWeek + $a) > $numOfDaysInMonth)
+            {
+                oneDay($dayInWeek + $a - $numOfDaysInMonth, $monthNumber + 1, $yearNumber, true);
+            }
+            else
+            {
+                oneDay($dayInWeek + $a, $monthNumber, $yearNumber, true);
+            }
+        }
+    }
+    // ------ End Days After ------
+    echo '</div>';
+
+
+
+    /*
+        echo '<div class="row" style="height: 70vh">';
+        for ($i = 14; $i <= 21; $i++)
+        {
+            oneDay($i, 6, 2022, true);
+        }
+        echo '</div>';
+    */
 }
