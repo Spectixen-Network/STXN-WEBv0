a027550f8ad05918a95dc848f985bd5e6cfae4e1
AppleJamp <enzoscottt3@gmail.com>
2022-07-15 14:47:30 +0200

    Update stx_1.sql

--- else/stx_1.sql
+++ else/stx_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Čtv 16. čen 2022, 21:08
+-- Vytvořeno: Pát 15. čec 2022, 14:43
 -- Verze serveru: 10.4.22-MariaDB
 -- Verze PHP: 8.1.2
 
@@ -58,8 +58,19 @@ CREATE TABLE `calendar_event` (
   `event_id` int(10) NOT NULL,
   `event_name` varchar(255) NOT NULL,
   `event_description` text DEFAULT NULL,
-  `event_date` date NOT NULL
+  `event_date` date NOT NULL,
+  `event_from` varchar(255) NOT NULL,
+  `event_to` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vypisuji data pro tabulku `calendar_event`
+--
+
+INSERT INTO `calendar_event` (`uid`, `event_id`, `event_name`, `event_description`, `event_date`, `event_from`, `event_to`) VALUES
+(2, 22, 'Dyk more prachy dojdu', 'U ARE BROKE HOE TILL THIS DAY', '2022-06-20', '00:00', '23:59'),
+(1, 23, 'My Birthday', '', '2022-08-01', '00:00', '23:59'),
+(1, 37, 'Mum&#039;s birthday', '', '2022-07-01', '00:00', '23:59');
 
 -- --------------------------------------------------------
 
@@ -72,8 +83,23 @@ CREATE TABLE `calendar_tags` (
   `tag_id` int(10) UNSIGNED NOT NULL,
   `tag_name` varchar(255) NOT NULL,
   `tag_description` text DEFAULT NULL,
-  `tag_color` varchar(255) DEFAULT '#fc03c6'
+  `tag_color` varchar(255) DEFAULT '#ffff'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vypisuji data pro tabulku `calendar_tags`
+--
+
+INSERT INTO `calendar_tags` (`user_id`, `tag_id`, `tag_name`, `tag_description`, `tag_color`) VALUES
+(1, 1, 'TEST', NULL, '#fc03c6'),
+(3, 2, 'Test', NULL, '#ffff'),
+(1, 3, 'Testing', '', '#321ec8'),
+(1, 4, 'Testing', '', '#321ec8'),
+(1, 5, 'ts', '', '#d6e31c'),
+(1, 6, 'sda', '', '#883a79'),
+(1, 7, 'TestTAg', '', '#65d1ec'),
+(2, 8, 'MONEH', 'The big $$$$$', '#3ee302'),
+(1, 9, 'Birthdays', '', '#c81414');
 
 -- --------------------------------------------------------
 
@@ -82,9 +108,19 @@ CREATE TABLE `calendar_tags` (
 --
 
 CREATE TABLE `event_tag` (
+  `uid` int(10) UNSIGNED NOT NULL,
   `tag_id` int(10) UNSIGNED NOT NULL,
   `event_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vypisuji data pro tabulku `event_tag`
+--
+
+INSERT INTO `event_tag` (`uid`, `tag_id`, `event_id`) VALUES
+(2, 8, 22),
+(1, 9, 23),
+(1, 9, 37);
 
 -- --------------------------------------------------------
 
@@ -111,7 +147,9 @@ INSERT INTO `user` (`uid`, `username`, `password`, `email`, `image_path`, `admin
 (3, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@test.test', 'images/noavatar.png', 0),
 (4, 'test1', '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', 'test1@test1.test1', 'images/noavatar.png', 1),
 (5, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '', 'images/noavatar.png', 1),
-(6, 'testToBan', 'e0670e55dbf2e1d06fbd1eabc6100b43f44c75857832c44f7d5c0034454fe98b', 'testToBan@test.com', 'images/noavatar.png', 0);
+(6, 'testToBan', 'e0670e55dbf2e1d06fbd1eabc6100b43f44c75857832c44f7d5c0034454fe98b', 'testToBan@test.com', 'images/noavatar.png', 0),
+(7, 'testCssFile', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'testcss@test.cz', 'images/noavatar.png', 0),
+(8, 'csstest', '5e772cdfa81655670dfce8b2941c35dadc6a61289d75b339ce2791b2f037616c', 'csstest@test.cz', 'images/noavatar.png', 0);
 
 -- --------------------------------------------------------
 
@@ -163,7 +201,8 @@ ALTER TABLE `calendar_tags`
 --
 ALTER TABLE `event_tag`
   ADD KEY `tag_id` (`tag_id`),
-  ADD KEY `event_id` (`event_id`);
+  ADD KEY `event_id` (`event_id`),
+  ADD KEY `uid` (`uid`);
 
 --
 -- Indexy pro tabulku `user`
@@ -186,19 +225,19 @@ ALTER TABLE `userinfo`
 -- AUTO_INCREMENT pro tabulku `calendar_event`
 --
 ALTER TABLE `calendar_event`
-  MODIFY `event_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `event_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT pro tabulku `calendar_tags`
 --
 ALTER TABLE `calendar_tags`
-  MODIFY `tag_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `tag_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pro tabulku `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Omezení pro exportované tabulky
@@ -233,7 +272,8 @@ ALTER TABLE `calendar_tags`
 --
 ALTER TABLE `event_tag`
   ADD CONSTRAINT `event_tag_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `calendar_tags` (`tag_id`),
-  ADD CONSTRAINT `event_tag_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `calendar_event` (`event_id`);
+  ADD CONSTRAINT `event_tag_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `calendar_event` (`event_id`),
+  ADD CONSTRAINT `event_tag_ibfk_3` FOREIGN KEY (`uid`) REFERENCES `calendar_tags` (`user_id`);
 
 --
 -- Omezení pro tabulku `userinfo`
