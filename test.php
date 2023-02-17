4f9a52b35c9a71f407f84bc274176860165e0591
AppleJamp <enzoscottt3@gmail.com>
2023-02-17 22:48:55 +0100

    test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = 'Účtenka č. KA-2021115220\nDodavatel:\nFANSHOP FC Banik Ostrava\nS.L.O.\nIČO 08666580\nBukovanskél a 1342\nDIC\n71000 Ostrava\nCz0a666580\nPIovozovna 12, pokladna: KA režan béžny\nBKP 415bed28 69117bbe 1911971d buc31bc8\nib489992\nFIK Obcofbc40lab 495c b832 a37h9643a674 04\nOdtèratel Sleva 15 pro držite e pertmarentky\nKartou: 357 KČ\nDatum a tas vystaveni 18 2. 2022 19:43:22\nE Vystavil Adam Zmzlik\nPoznámka:\nProdukt\nMJ\nCelkem\nTriko panske čeme Buzaly\n100let vel XXXXL FC Bank\nOstrava\n1 ks\n331 KC\npivodni cena 331 Kc sleva 0 KC (0%)\nEnergy dook FC Banik Osttava 1 ks\n26 Kč\npůvodní cena 2E KE sleva 0 Kč (0%)\nCeikem bez DPH\nDPH 15%\n3 KC\nDPH 21%\n58 KC\nColkem včeine DPH\n357 Kč\nvylvoleno vsystemu www llexos\n';
+
+echo $str;
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+echo strtoupper($str);
